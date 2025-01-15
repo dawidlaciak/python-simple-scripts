0380e6824c9a48ef7eb34a50c04da91b1e1d0fe1
Dawid Łaciak <dawidlaciak02@gmail.com>
2025-01-15 09:54:09 +0100

    updated main script

--- polygon_calculations/polygon_calculator.py
+++ polygon_calculations/polygon_calculator.py
@@ -3,32 +3,26 @@ import math
 def polygon_diagonals(polygon_sides:int) -> int:
     diagonals = round((polygon_sides * (polygon_sides - 3)) / 2)
     print(f'The number of diagonals of the polygon: {diagonals}')
-    return diagonals
 
 def polygon_interior_angles_sum(polygon_sides:int) -> int:
     angles_sum = 180 * (polygon_sides - 2)
     print(f'The sum of the interior angles of a polygon: {angles_sum}°')
-    return angles_sum
 
 def polygon_interior_angle(polygon_sides:int) -> float:
     interior_angle = round(((polygon_sides - 2) * 180) / polygon_sides, 2)
     print(f'Interior angle of polygon: {interior_angle}°')
-    return interior_angle
 
 def circumscribed_circle_radius(polygon_sides:int, side_length:float) -> float:
     radius = round(side_length / (2 * math.sin(math.pi / polygon_sides)), 2)
     print(f'Radius of the circumscribed circle: {radius}')
-    return radius
 
 def inscribed_circle_radius(polygon_sides:int, side_length:float) -> float:
     radius = round(side_length / (2 * math.tan(math.pi / polygon_sides)), 2)
     print(f'Radius of the circle inscribed in a polygon: {radius}')
-    return radius
 
 def area_of_polygon(polygon_sides:int, side_length:float) -> float:
     area = round((polygon_sides * side_length**2) / (4 * math.tan(math.pi / polygon_sides)), 2)
     print(f'Area of the polygon: {area}')
-    return area
 
 def polygon_all_data(polygon_sides:int, side_length:float) -> str:
     polygon_diagonals(polygon_sides)
