576bf63a824e0080c628e92f71b342adead498e2
dawidlaciak <dawidlaciak02@gmail.com>
2025-01-17 08:53:01 +0100

    added new dart_throw script

---
+++ dart_throw/darts.py
@@ -0,0 +1,15 @@
+def increase_points_dartboard():
+    
+    
+def generate_dartboard():
+    dartboard_row = [0 for i in range(13)]
+    print(type(dartboard_row))
+    
+    
+    for i in range(len(dartboard_row)):
+        print(' '.join(map(str, dartboard_row)))
+        dartboard_row = map(, dartboard_row)
+
+
+
+generate_dartboard()
